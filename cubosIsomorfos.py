3d7952e0a19748315cd3ac08254137f0df43a9be
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-23 22:57:17 -0300

    uploading cubosIsomorfos

--- cubosIsomorfos.py
+++ cubosIsomorfos.py
@@ -1,33 +1,15 @@
        
-#girando pra esquerda com as posiçoes 1 e 6 fixas (eixo z fixo)
-def giraParaEsquerdaEixoZFixo(cubo1):
-    cubo1 = cubo1[0]+cubo1[2]+cubo1[4]+cubo1[1]+cubo1[3]+cubo1[5]
-    return cubo1
+#girando com as posiçoes 1 e 6 fixas (eixo z fixo)
+def giraComEixoZFixo(cubo1):
+    return cubo1[0]+cubo1[2]+cubo1[4]+cubo1[1]+cubo1[3]+cubo1[5]
 
-#girando pra direita com as posiçoes 1 e 6 fixas (eixo z fixo)
-def giraParaDireitaEixoZFixo(cubo1):
-    cubo1 = cubo1[0]+cubo1[3]+cubo1[1]+cubo1[4]+cubo1[2]+cubo1[5]
-    return cubo1
+#girando com as posiçoes 2 e 5 fixas (eixo y fixo)
+def giraComEixoYFixo(cubo1):
+    return cubo1[3]+cubo1[1]+cubo1[0]+cubo1[5]+cubo1[4]+cubo1[2]
 
-#girando pra esquerda com as posiçoes 2 e 5 fixas (eixo y fixo)
-def giraParaEsquerdaEixoYFixo(cubo1):
-    cubo1 = cubo1[3]+cubo1[1]+cubo1[0]+cubo1[5]+cubo1[4]+cubo1[2]
-    return cubo1
-
-#girando pra direita com as posiçoes 2 e 5 fixas (eixo y fixo)
-def giraParaDireitaEixoYFixo(cubo1):
-    cubo1 = cubo1[2]+cubo1[1]+cubo1[5]+cubo1[0]+cubo1[4]+cubo1[3]
-    return cubo1
-
-#girando pra cima com as posiçoes 3 e 4 fixas (eixo x fixo)
-def giraParaCimaEixoXFixo(cubo1):
-    cubo1 = cubo1[1]+cubo1[5]+cubo1[2]+cubo1[3]+cubo1[0]+cubo1[4]
-    return cubo1
-
-#girando pra baixo com as posiçoes 3 e 4 fixas (eixo x fixo)
-def giraParaBaixoEixoXFixo(cubo1):
-    cubo1 = cubo1[4]+cubo1[0]+cubo1[2]+cubo1[3]+cubo1[5]+cubo1[1]
-    return cubo1
+#girando com as posiçoes 3 e 4 fixas (eixo x fixo)
+def giraComEixoXFixo(cubo1):
+    return cubo1[1]+cubo1[5]+cubo1[2]+cubo1[3]+cubo1[0]+cubo1[4]
 
 #giros possiveis e comparaçoes
 def cubosIsomorfos():
@@ -36,19 +18,18 @@ def cubosIsomorfos():
     while (nCasos > 0):
         cubo1 = input()
         cubo2 = input()
-        auxiliar = ""
-        for i in range(0,4):
-            auxiliar = giraParaEsquerdaEixoZFixo(cubo1)
-            auxiliar = giraParaDireitaEixoZFixo(auxiliar)
-            for j in range(0,4):
-                auxiliar = giraParaEsquerdaEixoYFixo(auxiliar)
-                auxiliar = giraParaDireitaEixoYFixo(auxiliar)
-                for k in range(0,4):
-                    auxiliar = giraParaCimaEixoXFixo(auxiliar)
-                    auxiliar = giraParaBaixoEixoXFixo(auxiliar)
-        print(auxiliar)
+        iguais = False
         
-        if (auxiliar == cubo2):
+        for i in range(0,4):
+            cubo1 = giraComEixoZFixo(cubo1)
+            for j in range(0,4):
+                cubo1 = giraComEixoYFixo(cubo1)
+                for k in range(0,4):
+                    cubo1 = giraComEixoXFixo(cubo1)
+                    if (cubo1 == cubo2):
+                        iguais = True
+  
+        if (iguais):
             print("S")
         else:
             print("N")
