2fd8250b27f106d718b55d9d5206f41ef07083b2
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-23 23:10:51 -0300

    creating origami

---
+++ origami.py
@@ -0,0 +1,8 @@
+def origami():
+    nCasos = int(input())
+    
+    while (nCasos > 0):
+        entrada = input().split(" ")
+        a = int(entrada[0])
+        b = int(entrada[1])
+        #continuar codigo
