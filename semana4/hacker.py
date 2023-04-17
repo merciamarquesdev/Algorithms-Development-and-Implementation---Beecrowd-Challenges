7f524ad9bdfd190f2a5d7aee7c5acc02e7097fa5
Mercia <68623425+mercietc@users.noreply.github.com>
2023-04-16 22:17:21 -0300

    creating hacker

---
+++ semana4/hacker.py
@@ -0,0 +1,31 @@
+import sys 
+sys.setrecursionlimit(10000)
+
+def hacker():
+    nCasos = int(input())    
+    while (nCasos > 0):
+        entrada = input().split(" ")
+        k = int(entrada[0])
+        n = int(entrada[1])
+        var = n - k
+        resultado = arranjo(n,k)
+        print(resultado)
+        
+        nCasos -= 1
+
+def multMod(a,b):
+    num = 1000000007
+    return (a*b)%num
+
+
+def arranjo(n,k):
+    result = 1
+    for i in range(1,n+1):
+        result *= multMod(k,k)
+    return result
+    #if(n == var):
+     #   return 1    
+    #else:
+     #   return multMod(n,arranjo(n-1,var))
+
+hacker()
