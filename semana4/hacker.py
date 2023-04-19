dcfdbf6e0c1f4f2e6d18eba8eff76f6cf61369ec
Mercia <68623425+mercietc@users.noreply.github.com>
2023-04-18 22:51:43 -0300

    Update hacker.py

--- semana4/hacker.py
+++ semana4/hacker.py
@@ -1,31 +1,29 @@
 import sys 
 sys.setrecursionlimit(10000)
 
+def multMod(a,b):
+    num = 1000000007
+    return (a*b)%num
+
+def arranjo(n,k):
+    if (n == 1):
+        return k
+    if (n%2 == 0):
+        result = arranjo(n/2,k)
+        return multMod(result,result)
+    else:
+        return multMod(k,arranjo(n-1,k))
+
 def hacker():
     nCasos = int(input())    
     while (nCasos > 0):
         entrada = input().split(" ")
         k = int(entrada[0])
         n = int(entrada[1])
-        var = n - k
         resultado = arranjo(n,k)
         print(resultado)
-        
         nCasos -= 1
-
-def multMod(a,b):
-    num = 1000000007
-    return (a*b)%num
-
-
-def arranjo(n,k):
-    result = 1
-    for i in range(1,n+1):
-        result *= multMod(result,k)
-    return result
-    #if(n == var):
-     #   return 1    
-    #else:
-     #   return multMod(n,arranjo(n-1,var))
-
+        
 hacker()
+
+
