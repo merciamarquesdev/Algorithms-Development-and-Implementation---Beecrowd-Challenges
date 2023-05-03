60d7abd004d2b239ddd15a56d8ec1a5dec8dd887
Mercia <68623425+mercietc@users.noreply.github.com>
2023-05-03 17:15:23 -0300

    Update bicicletaErgometrica.py

--- semana6/bicicletaErgometrica.py
+++ semana6/bicicletaErgometrica.py
@@ -2,14 +2,14 @@ def somaMod(a,b):
     num = 1000000007
     return (a+b)%num
 
-def T(i,j):
-    total = T(i,d)
+def T(i,j,m,n):
+    #total = T(i,d)
     if (j == 1 and i >= m and i <= n):
         return 1
     elif (i < m or i > n):
         return 0
     elif (j > 1 and i >= m and i <= n):
-        return somaMod(T(i-1,j-1),T(i+1,j-1))
+        return somaMod(T(i-1,j-1,m,n),T(i+1,j-1,m,n))
 
 def bicicletaErgonometrica():
     nCasos = int(input())
@@ -18,10 +18,13 @@ def bicicletaErgonometrica():
         t = int(entrada[0])
         m = int(entrada[1])
         n = int(entrada[2])
+        niveis = []
+        for i in range(m,n+1):
+            niveis.append(i)
         j = t
         total = 0
         for i in range(m,n):
-            total += T(i,j)
+            total += T(i,j,m,n)
         print(total)       
         
         nCasos -= 1
