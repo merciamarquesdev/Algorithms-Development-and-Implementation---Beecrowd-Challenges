0f0e3aff6c4a9fa03ed2db6d866eee6de002b339
Mercia <68623425+mercietc@users.noreply.github.com>
2023-05-08 23:53:16 -0300

    Update bicicletaErgometrica.py

--- semana6/bicicletaErgometrica.py
+++ semana6/bicicletaErgometrica.py
@@ -3,7 +3,7 @@ def somaMod(a,b):
     return (a+b)%num
 
 def T(i,j,m,n):
-    #total = T(i,d)
+    total = T(i,d)
     if (j == 1 and i >= m and i <= n):
         return 1
     elif (i < m or i > n):
