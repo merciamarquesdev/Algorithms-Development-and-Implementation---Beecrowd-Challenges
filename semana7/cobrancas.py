0a89e96029d92fedb6e7d93720afde060520c166
Mercia <68623425+mercietc@users.noreply.github.com>
2023-05-17 14:11:53 -0300

    Update cobrancas.py

--- semana7/cobrancas.py
+++ semana7/cobrancas.py
@@ -1,36 +1,47 @@
-def mochila(n,d,p,k):
-    k[0] = 0
-    for q in range(1,(n*2)+1,2):
-        for j in range(d,-1,-1):
-            if (k[j] >= 0):
-                if (j+p[q] <= d):
-                    if (k[j+p[q]] == -1):
-                        k[j+p[q]] = q
-                if (j+p[q+1] <= d):
-                    if (k[j+p[q+1]] == -1):
-                        k[j+p[q+1]] = q
-    #completei k
-    if (k[d] != -1):
-        maiorValor = 'S'
+def mochila(n,d,distancias,valores):
+    #criando vetor k, da mochila, e vetor auxiliar
+    k = []
+    aux = []
+    k.append(0)
+    aux.append(0)
+    for i in range(0,d):
+        k.append(-1)
+        aux.append(-1)
+    #comparacoes
+    for i in range(n):
+        for j in range(d,distancias[i]-1,-1):
+            if (k[j-distancias[i]] >= 0):
+                if (aux[j-distancias[i]] + valores[i] > aux[j]):
+                    k[j] = i + 1
+                    aux[j] = aux[j-distancias[i]] + valores[i]
+    #achando maior valor
+    maiorValor = 0
+    for i in range(d+1):
+        if (aux[i] > maiorValor):
+            maiorValor = aux[i]
     return maiorValor
+    
 
 def cobrancas():
     nCasos = int(input())
     while(nCasos > 0):
         entrada = input().split(" ")
         n = int(entrada[0])
-        d = int(int(entrada[1])/2) #considerar a distancia para voltar para casa
+        d = int(entrada[1]) #considerar a distancia para voltar para casa
         p = input().split(" ")
         #transformando p em vetor de inteiros
-        p.insert(0,0)
-        for i in range((2*n)+1):
+        for i in range(2*n):
             p[i] = int(p[i])
-        #criando vetor k
-        k = []
-        for i in range(0,d+1):
-            k.append(-1)
+        #criando vetor das distancias
+        distancias = []
+        for i in range(0,(2*n),2):
+            distancias.append(p[i]*2)
+        #criando vetor dos valores
+        valores = []
+        for i in range(1,(2*n),2):
+            valores.append(p[i])
         #exibindo resultado
-        resultado = mochila(n,d,p,k)
+        resultado = mochila(n,d,distancias,valores)
         print(resultado)
         
         nCasos -= 1
