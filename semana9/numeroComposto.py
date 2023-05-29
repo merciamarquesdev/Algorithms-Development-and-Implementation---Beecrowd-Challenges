27ec5b45ecd27c6a823614fb25306c1a3c3b9ebf
Mercia <68623425+mercietc@users.noreply.github.com>
2023-05-29 11:09:34 -0300

    Create numeroComposto.py

---
+++ semana9/numeroComposto.py
@@ -0,0 +1,57 @@
+import math
+
+def ehPrimo(n, P, np):
+    for i in range(np): #1 a np+1
+        if(n%p[i] == 0):
+            if(n != P[i]):
+                return False
+            else:
+                return True
+    return True
+
+def fatora(n,P,np,F,nf):
+    nf = 0
+    for i in range(np): #1 a np+1
+        while(n%p[i] == 0):
+            nf += 1
+            F[nf] = P[i]
+            n = n/P[i]
+        if(n == 1 or P[i] >= [math.sqrt(n)]):
+            break
+    if(n != 1):
+        nf += 1
+        F[nf] = n
+    return F
+
+def divisores(n,F,nf,D,nd):
+    F[0],nd,D[1] = 1,1,1
+    for i in range(1,nf+1):
+        if(F[i] == F[i-1]):
+            k *= F[i]
+        else:
+            k = F[i]
+            nda = nd
+        for j in range(1,nda+1): 
+            nd += 1
+            D[nd] = k*D[j]
+    return D
+
+
+def ehComposto(n,primo,fatorado):
+    comp = True
+    if(n):
+        return 'S'
+    else:
+        return 'N'
+
+def numeroComposto():
+    nCasos = int(input())
+    while(nCasos > 0):
+        n = int(input())
+        primo = ehPrimo(n,P,np)
+        fatorado = fatora(n,P,np,F,nf)
+        resultado = ehComposto(n,primo,fatorado)
+        print(resultado)
+        nCasos -= 1
+
+numeroComposto()
