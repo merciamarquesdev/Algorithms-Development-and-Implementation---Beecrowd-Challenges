ab335b54afbecd06edaf75bf508906b8672f846d
Mercia <68623425+mercietc@users.noreply.github.com>
2023-06-07 16:53:17 -0300

    Create diarioJulieta.py

---
+++ semana11/diarioJulieta.py
@@ -0,0 +1,20 @@
+
+
+def chaveCesar(msg,palavra):
+    limite = ord('z')-ord('a')+1
+    for i in range(limite):
+        
+            
+    return plv
+
+def diarioJulieta():
+    nCasos = int(input())
+    while(nCasos > 0):
+        msg = input().split(" ")
+        palavra = input().split(" ")
+        resultado = chaveCesar(msg,palavra)
+
+        nCasos -= 1
+
+
+diarioJulieta()
