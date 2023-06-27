0df252e01db69a48584126dca787154c4e01f638
Mercia <68623425+mercietc@users.noreply.github.com>
2023-06-26 23:36:53 -0300

    contandoTriangulos.py update

--- Semana13-Geometria-Computacional-Continua-II/contandoTriangulos.py
+++ Semana13-Geometria-Computacional-Continua-II/contandoTriangulos.py
@@ -2,29 +2,43 @@ def igual(a,b):
     z = 0.000000001
     return abs(a-b) <= z
 
-def menorIgual(a,b):
-    return (a<b) or igual(a,b)
+def menor(a,b):
+    return (a<b)
 
 def area(a,b,c):
-    area = (a[0]*(b[1]-c[1])) - (b[0]*(a[1]-c[1])) + (c[0]*(a[1]-b[1]))
+    s = (a+b+c)/2
+    area = (s*(s-a)*(s-b)*(s-c))**(1/2)
     return abs(area)
+
+def tipoTriangulo(a,b,c,nr,na,no):
+    if(igual(c**2,(a**2 + b**2))):
+        nr += 1
+    elif(menor(c**2,(a**2 + b**2))):
+        na += 1
+    else:
+        no += 1
+    return nr,na,no
 
 def contandoTriangulos():
     casos = int(input())
     while(casos > 0):
+        nr = 0
+        na = 0
+        no = 0
         n = int(input())
         pontos = input().split(" ")
         for i in range(len(pontos)):
             pontos[i] = int(pontos[i])
-            
-        a = pontos[:2]
-        b = pontos[2:4]
-        c = pontos[4:6]
-        p = pontos[6:]
 
-        area1 = area(a,b,p)
-        area2 = area(a,c,p)
-        area3 = area(b,c,p)
-                    
+        for x in range(len(pontos)):
+            for y in range(len(pontos)):
+                for z in range(len(pontos)):
+                    a = y-x
+                    b = z-y
+                    c = x-z
+                    if(area(a,b,c) != 0):#teste de nao-colinearidade
+                        nr,na,no = tipoTriangulo(a,b,c,nr,na,no)                   
+        print(nr,na,no)
+
         casos -= 1
 contandoTriangulos()
